aac64acab7480b1cf2ee5cb6a1a81614b5dc3758
raed <raedchaker99@gmail.com>
2020-05-08 13:49:47 +0100

    'actualite_work'

--- src/modules/frontpages/containers/actualite/actualite.component.ts
+++ src/modules/frontpages/containers/actualite/actualite.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+/* tslint:disable */
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+
 @Component({
     selector: 'sb-actualite',
     templateUrl: './actualite.component.html',
@@ -8,24 +10,46 @@ import { Router } from '@angular/router';
     styleUrls: ['./actualite.component.scss'],
 })
 export class ActualiteComponent implements OnInit {
+    // error: string;
+    // @ts-ignore
+    uploadResponse = {status: '', message: '', filePath: ''};
+
     errorMessage = '';
     // @ts-ignore
     actuForm: FormGroup;
     // @ts-ignore
     selectedFile: File = null;
-    constructor() {
-    }
-    ngOnInit(): void {
+
+    constructor(private http: HttpClient) {
     }
 
-    onFileSelected({ event }: { event: any }) {
-        this.selectedFile = event.target.files[0] as File;
+    ngOnInit(): void {
+        this.actuForm = new FormGroup({
+            'file': new FormControl(null, {validators: [Validators.required]})
+        })
     }
+
+    onFileSelected({event}: { event: any }) {
+        this.selectedFile = event.target.files[0] as File;
+        this.actuForm.patchValue({file: this.selectedFile});
+        if (this.actuForm.get('file') !== null){
+        this.actuForm.get('file').updateValueAndValidity();
+    }
+    }
+
     onUpload() {
+        console.log(this.selectedFile.name);
         const fd = new FormData();
         fd.append('file', this.selectedFile, this.selectedFile.name);
-        /* this.http.post('', fd).subscribe(res => {
-             console.log(res);
-         });*/
+        fd.append('name', this.selectedFile.name);
+        console.log(fd);
+        this.http.post('http://localhost:3000/api/documents', fd, {
+            reportProgress: true,
+            observe: 'events'
+        })
+            .subscribe(res => {
+                console.log('sending');
+                console.log(res);
+            });
     }
 }
